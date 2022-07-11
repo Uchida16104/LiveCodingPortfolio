66b992ff3b1a695ce0ac2d5db47f1f451b3c005f
HU <uho02741358@gmail.com>
2022-07-11 22:43:57 +0900

    Add files via upload

--- SOFT/MIDI.py
+++ SOFT/MIDI.py
@@ -1,1 +1,3 @@
-p1 >>MidiOut([0,1,1,2,3,5,8,13],amp=1,dur=16,channel=0)
+p1 >>MidiOut([0,1,1,2,3,5,8,13],amp=1,dur=2,channel=0)
+
+p1.stop()
--- SOFT/MIDI.rb
+++ SOFT/MIDI.rb
@@ -1,18 +1,21 @@
+#FoxDot
 live_loop :midi0 do
   use_real_time
   note, velocity= sync "/midi:iacdriver_bus1:1/note_on"
-  use_synth :pluck
-  play note, amp: velocity
+  use_synth :kalimba
+  play note, amp: velocity/127
 end
+#TidalCycles
 live_loop :midi1 do
   use_real_time
   note, velocity= sync "/midi:iacdriver_bus2:1/note_on"
-  use_synth :kalimba
-  play note, amp: velocity
+  use_synth :pluck
+  play note, amp: velocity/127
 end
+#Orca
 live_loop :midi2 do
   use_real_time
   note, velocity= sync "/midi:iacdriver_bus3:1/note_on"
   use_synth :piano
-  play note, amp: velocity
+  play note, amp: velocity/127
 end
