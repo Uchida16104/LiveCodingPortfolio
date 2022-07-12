8d6523c3a46e61d8303ed540d5d9dd8c9019f05a
HU <uho02741358@gmail.com>
2022-07-12 22:08:25 +0900

    Add files via upload

--- SOFT/MIDI.rb
+++ SOFT/MIDI.rb
@@ -1,21 +1,18 @@
-#FoxDot
-live_loop :midi0 do
+live_loop :FoxDot do
   use_real_time
   note, velocity= sync "/midi:iacdriver_bus1:1/note_on"
   use_synth :kalimba
-  play note, amp: velocity/127
+  play note, amp: velocity
 end
-#TidalCycles
-live_loop :midi1 do
+live_loop :TidalCycles do
   use_real_time
   note, velocity= sync "/midi:iacdriver_bus2:1/note_on"
   use_synth :pluck
-  play note, amp: velocity/127
+  play note, amp: velocity
 end
-#Orca
-live_loop :midi2 do
+live_loop :Orca do
   use_real_time
   note, velocity= sync "/midi:iacdriver_bus3:1/note_on"
   use_synth :piano
-  play note, amp: velocity/127
+  play note, amp: velocity
 end
