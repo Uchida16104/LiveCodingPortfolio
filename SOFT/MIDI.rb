53e43c8e24383aa870a5e7e1b73fc210bf71db47
HU <uho02741358@gmail.com>
2022-07-13 22:14:35 +0900

    Add files via upload

--- SOFT/MIDI.rb
+++ SOFT/MIDI.rb
@@ -1,18 +1,31 @@
+n=0.5
 live_loop :FoxDot do
-  use_real_time
-  note, velocity= sync "/midi:iacdriver_bus1:1/note_on"
-  use_synth :kalimba
-  play note, amp: velocity
+  with_fx :ring_mod,freq:62 do
+    with_fx :vowel,voice:choose([0,1,2,3,4]),vowel_sound:choose([1,2,3,4,5]) do
+      use_real_time
+      note, velocity= sync "/midi:iacdriver_bus1:1/note_on"
+      use_synth :kalimba
+      play note, amp: velocity
+    end
+  end
 end
 live_loop :TidalCycles do
-  use_real_time
-  note, velocity= sync "/midi:iacdriver_bus2:1/note_on"
-  use_synth :pluck
-  play note, amp: velocity
+  with_fx :octaver,amp:n do
+    with_fx :nhpf,amp:n do
+      use_real_time
+      note, velocity= sync "/midi:iacdriver_bus2:1/note_on"
+      use_synth :pluck
+      play note, amp: velocity
+    end
+  end
 end
 live_loop :Orca do
-  use_real_time
-  note, velocity= sync "/midi:iacdriver_bus3:1/note_on"
-  use_synth :piano
-  play note, amp: velocity
+  with_fx :ixi_techno,amp:n do
+    with_fx :nlpf,amp:n do
+      use_real_time
+      note, velocity= sync "/midi:iacdriver_bus3:1/note_on"
+      use_synth :piano
+      play note, amp: velocity
+    end
+  end
 end
